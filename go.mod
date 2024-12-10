fa040e72bf9c18495dc726cbe57ac56e41836ece
David Sedláček <david.littlefarmer@gmail.com>
2024-12-10 17:47:45 +0100

    Xlog, Networks (#1)
    * xlog
    
    * networks
    
    * go mod tidy
    
    * lint
    
    * Makefile
    
    * make lint

--- go.mod
+++ go.mod
@@ -1,3 +1,34 @@
 module github.com/0xsequence/go-libs
 
 go 1.23.4
+
+require (
+	github.com/0xsequence/ethkit v1.29.7
+	github.com/0xsequence/go-sequence v0.43.6
+)
+
+require (
+	github.com/Microsoft/go-winio v0.6.2 // indirect
+	github.com/bits-and-blooms/bitset v1.14.3 // indirect
+	github.com/btcsuite/btcd/btcec/v2 v2.3.4 // indirect
+	github.com/consensys/bavard v0.1.22 // indirect
+	github.com/consensys/gnark-crypto v0.14.0 // indirect
+	github.com/crate-crypto/go-kzg-4844 v1.1.0 // indirect
+	github.com/deckarep/golang-set/v2 v2.6.0 // indirect
+	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.3.0 // indirect
+	github.com/ethereum/c-kzg-4844/bindings/go v0.0.0-20230126171313-363c7d7593b4 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/gorilla/websocket v1.5.3 // indirect
+	github.com/goware/breaker v0.1.2 // indirect
+	github.com/goware/logger v0.3.0 // indirect
+	github.com/goware/superr v0.0.2 // indirect
+	github.com/holiman/uint256 v1.3.1 // indirect
+	github.com/mmcloughlin/addchain v0.4.0 // indirect
+	github.com/supranational/blst v0.3.13 // indirect
+	golang.org/x/crypto v0.28.0 // indirect
+	golang.org/x/net v0.29.0 // indirect
+	golang.org/x/sync v0.8.0 // indirect
+	golang.org/x/sys v0.26.0 // indirect
+	golang.org/x/text v0.19.0 // indirect
+	rsc.io/tmplfunc v0.0.3 // indirect
+)
---
+++ xlog/xlog.go
@@ -0,0 +1,112 @@
+// Package xlog (eXtended log) extends log/slog with additional helper functions and types.
+package xlog
+
+import (
+	"fmt"
+	"log/slog"
+	"math/big"
+	"strconv"
+
+	"github.com/0xsequence/go-sequence/lib/prototyp"
+
+	"github.com/0xsequence/go-libs/networks"
+)
+
+// slog.Any("error", err)
+func Error(err error) slog.Attr {
+	return slog.Any("error", err)
+}
+
+// slog.Any("error", fmt.Errorf(format, args...))
+func Errorf(format string, args ...any) slog.Attr {
+	return slog.Any("error", fmt.Errorf(format, args...))
+}
+
+// slog.Uint64("id", ID)
+func ID(ID uint64) slog.Attr {
+	return slog.Uint64("id", ID)
+}
+
+// slog.Uint64("chainId", chainID)
+func ChainID(chainID uint64) slog.Attr {
+	return slog.Uint64("chainId", chainID)
+}
+
+// slog.Uint64("chainId", chainID)
+func ChainIDString(chainID string) slog.Attr {
+	id, _ := strconv.ParseUint(chainID, 10, 64)
+	return ChainID(id)
+}
+
+// slog.String("name", name)
+func ChainNetworkName(name string) slog.Attr {
+	return slog.String("name", name)
+}
+
+// slog.Group("network", ChainID(network.ChainID), ChainNetworkName(network.Name))
+func ChainIDNetwork(network *networks.Network) slog.Attr {
+	if network == nil {
+		return slog.Group("network",
+			ChainNetworkName("unknown"),
+		)
+	}
+
+	return slog.Group("network",
+		ChainID(network.ChainID),
+		ChainNetworkName(network.Name),
+	)
+}
+
+// slog.String("contractAddress", contractAddress.String())
+func ContractAddress(contractAddress prototyp.Hash) slog.Attr {
+	return slog.String("contractAddress", contractAddress.String())
+}
+
+// slog.String("collectionAddress", collectionAddress.String())
+func CollectionAddress(collectionAddress prototyp.Hash) slog.Attr {
+	return slog.String("collectionAddress", collectionAddress.String())
+}
+
+// slog.String("currencyAddress", currencyAddress.String())
+func CurrencyAddress(currencyAddress prototyp.Hash) slog.Attr {
+	return slog.String("currencyAddress", currencyAddress.String())
+}
+
+// slog.String("orderID", orderID)
+func OrderID(orderID string) slog.Attr {
+	return slog.String("orderID", orderID)
+}
+
+// slog.String("tokenId", tokenID)
+func TokenIDString(tokenID string) slog.Attr {
+	return slog.String("tokenId", tokenID)
+}
+
+// slog.String("tokenId", tokenID.String())
+func TokenID(tokenID prototyp.BigInt) slog.Attr {
+	return TokenIDString(tokenID.String())
+}
+
+// slog.String("tokenId", tokenID.String())
+func TokenIDBigInt(tokenID big.Int) slog.Attr {
+	return TokenIDString(tokenID.String())
+}
+
+// slog.String("entity", tokenID.String())
+func Entity(entity string) slog.Attr {
+	return slog.String("entity", entity)
+}
+
+// slog.String("source", source)
+func Source(source string) slog.Attr {
+	return slog.String("source", source)
+}
+
+// slog.Uint64("projectId", projectID)
+func ProjectID(projectID uint64) slog.Attr {
+	return slog.Uint64("projectId", projectID)
+}
+
+func Stringer[T fmt.Stringer](k string, v T) slog.Attr {
+	return slog.String(k, v.String())
+}
