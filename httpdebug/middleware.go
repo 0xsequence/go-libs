9c0280111d86084f16990c1e8c849a97ecacf41d
Vojtech Vitek <vv@horizon.io>
2025-06-04 22:45:28 +0200

    Refactor httpdebug, remove *Client

--- httpdebug/middleware.go
+++ httpdebug/middleware.go
@@ -1,21 +1,19 @@
 package httpdebug
 
 import (
-	"context"
 	"net/http"
 )
 
-// Middleware creates an HTTP middleware function that adds debug to context if the "debug" header has the expected value.
-func (c *Client) Middleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// Check the debug header
-		if r.Header.Get(c.cfg.Header) == c.cfg.Password {
-			// Add the expected key to the context
-			ctx := context.WithValue(r.Context(), ctxKey{}, struct{}{})
-			r = r.WithContext(ctx)
-		}
+// Middleware enables debug mode in the context when an incoming request
+// contains the given debug header.
+func Middleware(debugHeader Header) func(next http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			if r.Header.Get(debugHeader.Key) == debugHeader.Value {
+				r = r.WithContext(enableDebugMode(r.Context()))
+			}
 
-		// Continue with the next handler
-		next.ServeHTTP(w, r)
-	})
+			next.ServeHTTP(w, r)
+		})
+	}
 }
