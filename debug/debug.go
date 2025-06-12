26a7145067f3f7a577e305798600ef2fa2437ba9
David Sedláček <david.littlefarmer@gmail.com>
2025-06-12 15:50:01 +0200

    remove debug middleware method

--- debug/debug.go
+++ debug/debug.go
@@ -1,10 +1,6 @@
 package debug
 
 import (
-	"net/http"
-
-	"github.com/go-chi/chi/v5/middleware"
-
 	"github.com/0xsequence/go-libs/httpdebug"
 )
 
@@ -16,11 +12,4 @@ type Debug struct {
 type BasicAuth struct {
 	Username string `toml:"username"`
 	Password string `toml:"password"`
-}
-
-// Return middleware for basic auth on passed realm
-func (b BasicAuth) Middleware(realm string) func(next http.Handler) http.Handler {
-	return middleware.BasicAuth(realm, map[string]string{
-		b.Username: b.Password,
-	})
 }
