3fa68990a8fa61669952709829ef3c33c32d8a39
David Sedláček <david.littlefarmer@gmail.com>
2025-06-12 14:04:43 +0200

    debugger check settings

--- httpdebug/middleware.go
+++ httpdebug/middleware.go
@@ -7,12 +7,19 @@ import (
 // Middleware enables debug mode in the context when an incoming request
 // contains the given debug header.
 func Middleware(debugHeader Header) func(next http.Handler) http.Handler {
+	// If the debug header is not fully defined, just return a passthrough handler.
+	if debugHeader.Key == "" || debugHeader.Value == "" {
+		return func(next http.Handler) http.Handler {
+			return next
+		}
+	}
+
+	// Otherwise, wrap the handler and check for the header.
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			if r.Header.Get(debugHeader.Key) == debugHeader.Value {
 				r = r.WithContext(enableDebugMode(r.Context()))
 			}
-
 			next.ServeHTTP(w, r)
 		})
 	}
--- httpdebug/transport.go
+++ httpdebug/transport.go
@@ -9,13 +9,22 @@ import (
 // Transport propagates debug mode by adding the given debug header to outgoing
 // requests, if enabled in the context.
 func Transport(debugHeader Header) func(next http.RoundTripper) http.RoundTripper {
+	// If the debug header is not fully defined, just return a passthrough transport.
+	if debugHeader.Key == "" || debugHeader.Value == "" {
+		return func(next http.RoundTripper) http.RoundTripper {
+			return transport.RoundTripFunc(func(r *http.Request) (*http.Response, error) {
+				return next.RoundTrip(r)
+			})
+		}
+	}
+
+	// Wrap the transport to inject the header if debug mode is enabled.
 	return func(next http.RoundTripper) http.RoundTripper {
 		return transport.RoundTripFunc(func(r *http.Request) (*http.Response, error) {
 			if IsDebugModeEnabled(r.Context()) {
 				r = transport.CloneRequest(r)
 				r.Header.Set(debugHeader.Key, debugHeader.Value)
 			}
-
 			return next.RoundTrip(r)
 		})
 	}
