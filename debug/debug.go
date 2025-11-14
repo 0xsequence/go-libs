8b11be0a5110c5daba5b6a5d76e3f88982fa2c3a
Vojtech Vitek (golang.cz) <vv@sequence.xyz>
2025-11-14 18:00:15 +0100

    Add go-libs/config pkg (#16)

--- debug/debug.go
+++ debug/debug.go
@@ -4,11 +4,14 @@ import (
 	"github.com/0xsequence/go-libs/httpdebug"
 )
 
+// Deprecated: Use github.com/0xsequence/go-libs/config.Debug instead.
 type Debug struct {
+	Enabled   bool             `toml:"enabled"`
 	BasicAuth BasicAuth        `toml:"basic_auth"`
 	Header    httpdebug.Header `toml:"header"`
 }
 
+// Deprecated: Use github.com/0xsequence/go-libs/config.BasicAuth instead.
 type BasicAuth struct {
 	Username string `toml:"username"`
 	Password string `toml:"password"`
